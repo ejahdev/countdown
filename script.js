2ba3c849a8de6b531f57c38083e807a5c544c234
EjahDev <116228748+ejahdev@users.noreply.github.com>
2023-10-25 06:01:03 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+function updateTimer() {
+    const now = new Date();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+
+    // Calculate minutes and seconds remaining until the next :20 past the hour
+    let minutesRemaining;
+    let secondsRemaining;
+
+    if (minutes < 20) {
+        minutesRemaining = 20 - minutes;
+        secondsRemaining = 60 - seconds;
+    } else {
+        minutesRemaining = 80 - minutes; // 60 (full hour) + 20 - minutes
+        secondsRemaining = 60 - seconds;
+    }
+
+    // Determine whether to show "minute" or "minutes"
+    const minuteLabel = minutesRemaining === 1 ? "minute" : "minutes";
+    
+    // Construct the countdown message
+    let countdownText;
+    
+    if (minutesRemaining > 0) {
+        countdownText = `${minutesRemaining} ${minuteLabel} ${secondsRemaining} seconds left till the next 20`;
+    } else if (secondsRemaining > 0) {
+        const secondLabel = secondsRemaining === 1 ? "second" : "seconds";
+        countdownText = `${secondsRemaining} ${secondLabel} left till the next 20`;
+        
+        if (secondsRemaining === 60) {
+            // Apply the zooming animation when there are 60 seconds left
+            document.getElementById('countdown').classList.add('zooming');
+        } else {
+            // Remove the animation when it's not 60 seconds
+            document.getElementById('countdown').classList.remove('zooming');
+        }
+    } else {
+        countdownText = "Next 20 is here!";
+    }
+
+    // Update the elements by their IDs
+    document.getElementById('minutes').textContent = minutesRemaining > 0 ? minutesRemaining : '';
+    document.getElementById('seconds').textContent = secondsRemaining > 0 ? secondsRemaining : '';
+
+    document.getElementById('countdown').textContent = countdownText;
+}
+
+// Call updateTimer every second
+setInterval(updateTimer, 1000);
+
+// Initial call to set the timer when the page loads
+updateTimer();
