cd819e9eac6470c18493e45ee4b7680eda45e944
EjahDev <116228748+ejahdev@users.noreply.github.com>
2023-11-02 21:56:18 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -2,49 +2,39 @@ function updateTimer() {
     const now = new Date();
     const currentHour = now.getHours();
     const currentMinutes = now.getMinutes();
+    const currentSeconds = now.getSeconds();
     const targetHourPM = 16; // 4 PM
     const targetHourAM = 4;  // 4 AM
     const targetMinute = 20;
 
-    // Calculate the time until the next 4:20
     let targetHour, message;
 
     if (currentHour > targetHourPM || (currentHour === targetHourPM && currentMinutes > targetMinute)) {
-        // If it's past 4:20 PM, calculate the time until 4:20 AM of the next day
         targetHour = targetHourAM;
-        message = `till 4:20 AM!`;
+        message = `AM`;
     } else if (currentHour < targetHourAM || (currentHour === targetHourAM && currentMinutes < targetMinute)) {
-        // If it's before 4:20 AM, calculate the time until 4:20 AM of today
         targetHour = targetHourAM;
-        message = `till 4:20 AM!`;
+        message = `AM`;
     } else {
-        // If it's between 4:20 AM and 4:20 PM, calculate the time until 4:20 PM of today
         targetHour = targetHourPM;
-        message = `till 4:20 PM!`;
+        message = `PM`;
     }
 
-    // Calculate the time difference in milliseconds
-    const targetTime = new Date(now);
-    targetTime.setHours(targetHour, targetMinute, 0, 0);
-    const timeDiff = targetTime - now;
+    let hoursRemaining = targetHour - currentHour;
+    let minutesRemaining = currentMinutes > targetMinute ? 60 - currentMinutes + targetMinute : targetMinute - currentMinutes;
+    let secondsRemaining = 59 - currentSeconds;
 
-    // If the time difference is negative, it means it's already past the target time, so we need to adjust for the next occurrence
-    if (timeDiff < 0) {
-        targetTime.setDate(targetTime.getDate() + 1);
-        timeDiff = targetTime - now;
+    if (hoursRemaining < 0) {
+        hoursRemaining += 24;
     }
 
-    const hoursRemaining = Math.floor(timeDiff / 3600000);
-    const minutesRemaining = Math.floor((timeDiff % 3600000) / 60000);
-    const secondsRemaining = Math.floor((timeDiff % 60000) / 1000);
-
-    const countdownText = `${hoursRemaining > 0 ? `<span id="hours" class="countdown-num">${hoursRemaining}</span> hour${hoursRemaining > 1 ? 's ' : ' '}` : ''}${minutesRemaining > 0 ? `<span id="minutes" class="countdown-num">${minutesRemaining}</span> minute${minutesRemaining === 1 ? ' ' : 's '}` : ''}<span id="seconds" class="countdown-num">${secondsRemaining}</span> second${secondsRemaining === 1 ? ' ' : 's '}<br> till West Coast <span class="time-text">4:20</span>!`;
+    const countdownText = `${hoursRemaining > 0 ? `<span id="hours" class="countdown-num">${hoursRemaining}</span> hour${hoursRemaining > 1 ? 's ' : ' '}` : ''}${minutesRemaining > 0 ? `<span id="minutes" class="countdown-num">${minutesRemaining}</span> minute${minutesRemaining === 1 ? ' ' : 's '}` : ''}<span id="seconds" class="countdown-num">${secondsRemaining}</span> second${secondsRemaining === 1 ? ' ' : 's '}`;
 
     if (currentHour === targetHour && currentMinutes === targetMinute) {
-        countdownText = `CHEERS Happy 4:20!!`;
+        countdownText = `<span class="time-text">CHEERS Happy 4:20 ${message}</span>`;
     }
 
-    document.getElementById('countdown').innerHTML = countdownText + message;
+    document.getElementById('countdown').innerHTML = countdownText + `<br>till West Coast <span class="time-text">4:20</span> ${message}!`;
 }
 
 // Call updateTimer every second
