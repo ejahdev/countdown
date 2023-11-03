073f7b26c1c52f89797ec81f02650f3af83c9c06
EjahDev <116228748+ejahdev@users.noreply.github.com>
2023-11-02 21:33:35 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -2,20 +2,18 @@ function updateTimer() {
     const now = new Date();
     const currentHour = now.getHours();
     const currentMinutes = now.getMinutes();
-    const currentSeconds = now.getSeconds();
-
-    // Set the target hours for 4:20 AM and 4:20 PM
-    const targetHourAM = 4;
-    const targetHourPM = 16;
+    const targetHourPM = 16; // 4 PM
+    const targetHourAM = 4;  // 4 AM
     const targetMinute = 20;
 
+    // Calculate the time until the next 4:20
     let targetHour, message;
 
     if (currentHour > targetHourPM || (currentHour === targetHourPM && currentMinutes > targetMinute)) {
         // If it's past 4:20 PM, calculate the time until 4:20 AM of the next day
         targetHour = targetHourAM;
         message = `till 4:20 AM!`;
-    } else if (currentHour < targetHourAM || (currentHour === targetHourAM && currentMinutes <= targetMinute)) {
+    } else if (currentHour < targetHourAM || (currentHour === targetHourAM && currentMinutes < targetMinute)) {
         // If it's before 4:20 AM, calculate the time until 4:20 AM of today
         targetHour = targetHourAM;
         message = `till 4:20 AM!`;
@@ -40,13 +38,13 @@ function updateTimer() {
     const minutesRemaining = Math.floor((timeDiff % 3600000) / 60000);
     const secondsRemaining = Math.floor((timeDiff % 60000) / 1000);
 
-    const countdownText = `${hoursRemaining > 0 ? `<span id="hours" class="countdown-num" style="color: #6441A5">${hoursRemaining}</span> hour${hoursRemaining > 1 ? 's ' : ' '}` : ''}${minutesRemaining > 0 ? `<span id="minutes" class "countdown-num" style="color: #6441A5">${minutesRemaining}</span> minute${minutesRemaining === 1 ? ' ' : 's '}` : ''}<span id="seconds" class="countdown-num" style="color: #6441A5">${secondsRemaining}</span> second${secondsRemaining === 1 ? ' ' : 's '}<br> till West Coast <span class="time-text">4:20</span>!`;
+    const countdownText = `${hoursRemaining > 0 ? `<span id="hours" class="countdown-num">${hoursRemaining}</span> hour${hoursRemaining > 1 ? 's ' : ' '}` : ''}${minutesRemaining > 0 ? `<span id="minutes" class="countdown-num">${minutesRemaining}</span> minute${minutesRemaining === 1 ? ' ' : 's '}` : ''}<span id="seconds" class="countdown-num">${secondsRemaining}</span> second${secondsRemaining === 1 ? ' ' : 's '}<br> till West Coast <span class="time-text">4:20</span>!`;
 
     if (currentHour === targetHour && currentMinutes === targetMinute) {
         countdownText = `CHEERS Happy 4:20!!`;
     }
 
-    document.getElementById('countdown').innerHTML = countdownText;
+    document.getElementById('countdown').innerHTML = countdownText + message;
 }
 
 // Call updateTimer every second
